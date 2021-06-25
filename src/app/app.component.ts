1325d65d333c87ba874e38a5c7f4c7e81c421668
TXT-39 <txt-39@MacBook-Pro-42.attlocal.net>
2021-06-25 08:51:24 -0700

    Second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularLesson';
+  textToChange = 'Type something here, its kinda empty';
+  Alejandro = ["I like to play baseball", "I like to watch anime", "I like to draw", "I enjoy outdoor activites"];
+}
