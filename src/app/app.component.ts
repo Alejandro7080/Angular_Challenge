9cfd2bac5b7c023a7b2a1f6fec6ac5c6fbb4fd01
TXT-39 <txt-39@MacBook-Pro-42.attlocal.net>
2021-06-25 09:51:20 -0700

    4th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,3 @@ export class AppComponent {
   textToChange = 'Type something here, its kinda empty';
   Alejandro = ["My name is Alejandro", "I am 14 years old", "I like to watch anime", "I enjoy outdoor activites"];
 }
-export class Me {
-  name = 'Alejandro'
-  age = '14'
-  height = '5 feet, 7 inches'
-}
